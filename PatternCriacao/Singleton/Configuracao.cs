072e409d531fb028a6fde4208f32337bb214fbd2
Eli Pedro dos Santos <elipedro.santos@outlook.com>
2017-03-02 14:46:40 -0300

    OK

---
+++ PatternCriacao/Singleton/Configuracao.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Singleton
+{
+    public class Configuracao
+    {
+        private static Configuracao Instance { get; set; }
+        private Dictionary<string, string> Config { get; set; }
+
+        public Configuracao()
+        {
+            this.Config = new Dictionary<string, string>();
+            this.Config.Add("time-zone", "America/Sao_Paulo");
+            this.Config.Add("currency-code","BRL");
+        }
+
+        public static Configuracao GetInstance()
+        {
+            if(Configuracao.Instance == null)
+            {
+                Configuracao.Instance = new Configuracao();
+            }
+
+            return Configuracao.Instance;
+        }
+
+        public string GetPropriedade(string propriedade)
+        {
+            return Config[propriedade];        
+        }
+    }
+}
---
+++ PatternCriacao/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = Configuracao.GetInstance();
+
+            Console.WriteLine("---- Propriedades ----");
+            Console.WriteLine("Time-Zone - {0}", config.GetPropriedade("time-zone"));
+            Console.WriteLine("Currency-Code - {0}", config.GetPropriedade("currency-code"));
+
+            Console.ReadKey();
+        }
+    }
+}
